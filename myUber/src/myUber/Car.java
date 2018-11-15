4cebcdbd3d346748bdb3893f19c391bb88e4d542
aaxelv <44616547+aaxelv@users.noreply.github.com>
2018-11-15 17:37:05 +0100

    Update Car.java

--- myUber/src/myUber/Car.java
+++ myUber/src/myUber/Car.java
@@ -3,8 +3,13 @@ public interface Car {
 
 
 	public  void addDriver(Driver driver);
-	public String getGPS();
 	public  boolean contact();
+	
 	public String getID();
 	public int getnbplace();
+	public String getGPS();
+	
+	void create(String GPS);
+
 }
+
