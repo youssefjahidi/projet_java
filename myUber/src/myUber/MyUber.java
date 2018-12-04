96a3f615a762e5400b1b74e10bf329d5f90c8f6c
aaxelv <44616547+aaxelv@users.noreply.github.com>
2018-12-04 12:20:09 +0100

    Update MyUber.java

--- myUber/src/myUber/MyUber.java
+++ myUber/src/myUber/MyUber.java
@@ -24,33 +24,16 @@ public class MyUber {
 		}
 		return null;
 	};*/
-	/*public void note(int note) {
-	this.note = (this.note*nbcourse +note) /(nbcourse+1);
-	nbcourse++;
-}*/
-	
-	/*public void choose() {
-	Scanner sc = new Scanner(System.in);
-	System.out.println("Quel type de course choisissez vous?");
-	String RidesType = sc.nextLine();
-	System.out.println("Combien �tes vous?");
-	int nbpersonne = sc.nextInt();
-	Rides ride = RidesFactory.createRides(RidesType);
-	ride.prepare(this,this.destination, nbpersonne);
-	ride.request();	
-	this.rides = ride;
-}
-public void cancel() {
-		this.rides.cancel();  dans myuber
-	}
+	/**
+	 * Customer hashmap indexed by customer id
+	 */
+	public static HashMap<Integer, Customer> customers = new HashMap<Integer, Customer>();
 
-	public void note(int note) {
-		this.rides.note(note);  dans my uber
+	/**
+	 * Customer hashmap indexed by customer id
+	 */
+	public static HashMap<String[], Driver> drivers = new HashMap<String[], Driver>();
 
-public static ArrayList<String> BookOfRides = new ArrayList<String>();
-*/
-	
-	
 	/**
 	 * Cars hashmap indexed by car id
 	 */
@@ -71,6 +54,88 @@ public static ArrayList<String> BookOfRides = new ArrayList<String>();
 	 */
 	public static HashMap<String, Car> allCars = new HashMap<String, Car>();
 	
+	private CarFactory carFactory = new CarFactory();
+	
+	/**
+	 * Add a Customer in the system
+	 * @param name of the Customer
+	 * @param surname of the Customer
+	 */
+	public void addCustomer(String name,String surname) {
+		Customer customer = new Customer(name,surname);
+		MyUber.customers.put(customer.getId(), customer);
+		for (int key : MyUber.customers.keySet()) {
+			System.out.println("The " + key + " customer is " + 
+					MyUber.customers.get(key).getName() );
+			}
+	}
+	
+	/**
+	 * Add a Car and a Driver and associate them
+	 * @param Drivername the name of the driver
+	 * @param Driversurname the surname of the driver
+	 * @param carType the type of the car
+	 */
+	public void addCarDriver(String Drivername,String Driversurname,String carType) {
+		Driver driver = new Driver(Drivername, Driversurname);
+		Car car = carFactory.createCar(carType);
+		car.addDriver(driver);
+		driver.setCar(car);
+		String[] s = {Drivername,Driversurname};
+		MyUber.drivers.put(s, driver);
+		MyUber.allCars.put(car.getId(),car);
+		// Est ce que les vanCars/BerlineCar etc. sont vraiment utiles ? On pourrait simplement parcourir 
+		// allCars et checker si l'ID commence par van/berline etc. non ? Sinon c'est un peu chiant pour
+		// ajouter chaque nouvelle voiture une fois crée dans la bonne liste
+		// Et c'est un peu redondant		
+		for (String key : MyUber.allCars.keySet()) {
+			System.out.println("The car : " + key + " is associated with the following Driver : \n");
+					for (Driver driverofcar : MyUber.allCars.get(key).getDrivers()) {
+						System.out.println( "   " + driverofcar.getName()+" "+ driverofcar.getSurname()+ "\n");
+					};
+			}
+	}
+	
+	/**
+	 * Add a Driver and associate it with an existing Car
+	 * @param Drivername the name of the driver
+	 * @param Driversurname the surname of the driver
+	 * @param carID the ID of the car
+	 */
+	public void addDriver(String Drivername, String Driversurname,String carID) {
+		Driver driver = new Driver(Drivername, Driversurname);
+		Car car = MyUber.allCars.get(carID);
+		driver.setCar(car);
+		String[] s = {Drivername,Driversurname};
+		MyUber.drivers.put(s, driver);
+		for (String key : MyUber.allCars.keySet()) {
+			System.out.println("The car : " + key + " is associated with the following Driver : \n");
+					for (Driver driverofcar : MyUber.allCars.get(key).getDrivers()) {
+						System.out.println( "   " + driverofcar.getName()+" "+ driverofcar.getSurname()+ "\n");
+					};
+			}
+	}
+	
+	/**
+	 * Add a Driver and associate it with an existing Car
+	 * @param Drivername the name of the driver
+	 * @param Driversurname the surname of the Driver
+	 * @param status the status of the driver to be set
+	 */
+	public void setDriverStatus(String Drivername, String Driversurname,String status) {
+		DriverState state = DriverState // je sais pas comment manipuler les enum, mais j'ai 
+				// l'impression que c'est pas super pratique après des rapides recherches
+				// Tu pourras voir comment gérer ça ?
+		String[] s = {Drivername,Driversurname};
+		MyUber.drivers.get(s).setState(state);
+		for (String key : MyUber.allCars.keySet()) {
+			System.out.println("The car : " + key + " is associated with the following Driver : \n");
+					for (Driver driverofcar : MyUber.allCars.get(key).getDrivers()) {
+						System.out.println( "   " + driverofcar.getName()+" "+ driverofcar.getSurname()+ "\n");
+					};
+			}
+	}
+	
 	/**
 	 * Change car's gps position
 	 * @param carId car id 
@@ -78,15 +143,18 @@ public static ArrayList<String> BookOfRides = new ArrayList<String>();
 	 * @param posY y coordinate
 	 * @return Boolean: true if id found and coordinates inside the bounds, else returns false.
 	 */
-	public boolean moveCar(String carId, float posX, float posY){
+	public boolean moveCar(String carId, GPSPosition gpsPosition){
 		allCars.putAll(standardCars);
 		allCars.putAll(berlineCars);
 		allCars.putAll(vanCars);		
-		if(allCars.containsKey(carId) && Math.abs(posX)<=50 && Math.abs(posY)<=50 ){
-			allCars.get(carId).setGpsPositionX(posX);
-			allCars.get(carId).setGpsPositionY(posY);
+		if(allCars.containsKey(carId) && Math.abs(gpsPosition.getX())<=50 && Math.abs(gpsPosition.getY())<=50 ){
+			allCars.get(carId).setGpsPosition(gpsPosition);
 			return true;}
 		else{return false;}
 	}
 
+	
+	public boolean setup(int nBerlineCar,int nVanCar, int nCustomer) {
+		return true;
+	}
 }
